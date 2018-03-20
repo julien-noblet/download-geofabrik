2c94ba1acf7af9cf38e4817d2f62062ac609e818
Julien Noblet <julien.noblet@gmail.com>
2018-03-20 13:10:33 +0100

    More tests for download-geofabrik

--- download-geofabrik_test.go
+++ download-geofabrik_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"testing"
 )
 
@@ -71,6 +72,107 @@ func Test_catch(t *testing.T) {
 			}()
 			catch(tt.args.err)
 			t.Errorf("The code did not panic")
+		})
+	}
+}
+
+func Test_hashFileMD5(t *testing.T) {
+	type args struct {
+		filePath string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{name: "Check with LICENSE file", args: args{filePath: "./LICENSE"}, want: "65d26fcc2f35ea6a181ac777e42db1ea", wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := hashFileMD5(tt.args.filePath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("hashFileMD5(%v) error = %v, wantErr %v", tt.args.filePath, err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("hashFileMD5() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Benchmark_hashFileMD5_LICENSE(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		hashFileMD5("./LICENSE")
+	}
+}
+func Benchmark_controlHash_LICENSE(b *testing.B) {
+	hash, _ := hashFileMD5("./LICENSE")
+	hashfile := "/tmp/download-geofabrik-test.hash"
+	ioutil.WriteFile(hashfile, []byte(hash), 0644)
+	for n := 0; n < b.N; n++ {
+		controlHash(hashfile, hash)
+	}
+}
+
+func Test_controlHash(t *testing.T) {
+	type args struct {
+		hashfile string
+		hash     string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		want       bool
+		wantErr    bool
+		fileToHash string
+	}{
+		// TODO: Add test cases.
+		{name: "Check with LICENSE file", fileToHash: "./LICENSE", args: args{hashfile: "/tmp/download-geofabrik-test.hash", hash: "65d26fcc2f35ea6a181ac777e42db1ea"}, want: true, wantErr: false},
+		{name: "Check with LICENSE file wrong hash", fileToHash: "./LICENSE", args: args{hashfile: "/tmp/download-geofabrik-test.hash", hash: "65d26fcc2f35ea6a181ac777e42db1eb"}, want: false, wantErr: false},
+	}
+	for _, tt := range tests {
+		hash, _ := hashFileMD5(tt.fileToHash)
+		ioutil.WriteFile(tt.args.hashfile, []byte(hash), 0644)
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := controlHash(tt.args.hashfile, tt.args.hash)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("controlHash() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("controlHash() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_downloadChecksum(t *testing.T) {
+	type args struct {
+		format string
+	}
+	tests := []struct {
+		name     string
+		fConfig  string
+		dCheck   bool
+		delement string
+		args     args
+		want     bool
+	}{
+		// TODO: Add test cases.
+		{name: "dCheck = false", dCheck: false, want: false},
+		{name: "dCheck = true andorra.osm.pbf from geofabrik", fConfig: "./geofabrik.yml", dCheck: true, delement: "andorra", args: args{format: "osm.pbf"}, want: false},
+	}
+	for _, tt := range tests {
+		*dCheck = tt.dCheck
+		*fConfig = tt.fConfig
+		*delement = tt.delement
+		t.Run(tt.name, func(t *testing.T) {
+			if got := downloadChecksum(tt.args.format); got != tt.want {
+				t.Errorf("downloadChecksum() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
