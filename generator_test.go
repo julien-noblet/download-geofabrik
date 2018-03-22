996d45042283b90be83cbbc1188f1b4f79e532c0
Julien Noblet <julien.noblet@gmail.com>
2018-03-22 11:28:08 +0100

    test(generator): Add handle error and add some tests for generator

--- generator_test.go
+++ generator_test.go
@@ -4,7 +4,8 @@ import (
 	"reflect"
 	"testing"
 
-	"gopkg.in/yaml.v2"
+	"github.com/PuerkitoBio/gocrawl"
+	yaml "gopkg.in/yaml.v2"
 )
 
 func TestElementSlice_Generate(t *testing.T) {
@@ -58,6 +59,77 @@ func TestGenerate(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			Generate(tt.args.configfile)
+		})
+	}
+}
+
+func TestExt_mergeElement(t *testing.T) {
+	myFakeGeorgia := sampleFakeGeorgia2Ptr
+	myFakeGeorgia.ID = "georgia-us"
+	type args struct {
+		element *Element
+	}
+	tests := []struct {
+		name    string
+		fields  Ext
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "adding a new element",
+			fields: Ext{
+				DefaultExtender: new(gocrawl.DefaultExtender),
+				Elements:        sampleElementValidPtr,
+			},
+			args: args{
+				element: &sampleFakeGeorgiaPtr,
+			},
+			wantErr: false,
+		},
+		{
+			name: "adding a same element",
+			fields: Ext{
+				DefaultExtender: new(gocrawl.DefaultExtender),
+				Elements:        sampleElementValidPtr,
+			},
+			args: args{
+				element: &sampleAfricaElementPtr,
+			},
+			wantErr: false,
+		},
+		{
+			name: "adding a meta element",
+			fields: Ext{
+				DefaultExtender: new(gocrawl.DefaultExtender),
+				Elements:        sampleElementValidPtr,
+			},
+			args: args{
+				element: &sampleUsElementPtr,
+			},
+			wantErr: false,
+		},
+		{
+			name: "adding a same element parent differ",
+			fields: Ext{
+				DefaultExtender: new(gocrawl.DefaultExtender),
+				Elements:        sampleElementValidPtr,
+			},
+			args: args{
+				element: &myFakeGeorgia,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := &Ext{
+				DefaultExtender: tt.fields.DefaultExtender,
+				Elements:        tt.fields.Elements,
+			}
+			if err := e.mergeElement(tt.args.element); (err != nil) != tt.wantErr {
+				t.Errorf("Ext.mergeElement() error = %v, wantErr %v", err, tt.wantErr)
+			}
 		})
 	}
 }
