63f01a722573e7eb920317d260edb690c3c4a1a2
Julien Noblet <julien.noblet@gmail.com>
2019-07-04 08:04:45 +0200

    fix gosimple errors

--- openstreetmap.fr.go
+++ openstreetmap.fr.go
@@ -150,7 +150,7 @@ func (o *OpenstreetmapFR) parse(e *colly.HTMLElement, c *colly.Collector) {
 			if err != colly.ErrNoURLFiltersMatch {
 				catch(err)
 			} else {
-				if *fVerbose && *fProgress == false && *fQuiet == false {
+				if *fVerbose && !*fProgress && !*fQuiet {
 					log.Printf("URL: %v is not matching URLFilters\n", href)
 				}
 			}
