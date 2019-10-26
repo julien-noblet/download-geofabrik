b4706997e584068c877f8a8bb0fa4badc82be246
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-26 19:51:19 +0000

    Deploy julien-noblet/download-geofabrik to github.com/julien-noblet/download-geofabrik.git:julien-noblet-patch-1

--- config.go
+++ config.go
@@ -1,4 +1,4 @@
-// 2015-2019 copyright Julien Noblet
+// 2015-2018 copyright Julien Noblet
 
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
