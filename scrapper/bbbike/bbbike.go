4e9d6710bc2d181c378f027d300567f8a67b265a
Julien Noblet <julien.noblet@gmail.com>
2024-09-25 22:43:36 +0200

    Refactor Bbbike scrapper code and update default configuration
    - Refactor the Bbbike scrapper code to improve readability and maintainability.
    - Update the default configuration for the Bbbike scrapper with new constants for magic numbers.

--- scrapper/bbbike/bbbike.go
+++ scrapper/bbbike/bbbike.go
@@ -11,17 +11,24 @@ import (
 	"github.com/julien-noblet/download-geofabrik/scrapper"
 )
 
+// Constants for magic numbers.
+const (
+	progressBarCount = 237
+	parallelism      = 20
+)
+
 // Bbbike Scrapper.
 type Bbbike struct {
 	*scrapper.Scrapper
 }
 
+// GetDefault returns the default configuration for Bbbike scrapper.
 func GetDefault() *Bbbike {
 	return &Bbbike{
-		Scrapper: &scrapper.Scrapper{ //nolint:exhaustruct // I'm lazy
-			PB:             237, //nolint:gomnd // there is 237 element at this time
+		Scrapper: &scrapper.Scrapper{
+			PB:             progressBarCount,
 			Async:          true,
-			Parallelism:    20, //nolint:gomnd // Use 20 threads for scraping
+			Parallelism:    parallelism,
 			MaxDepth:       0,
 			AllowedDomains: []string{`download.bbbike.org`},
 			BaseURL:        `https://download.bbbike.org/osm/bbbike`,
@@ -31,24 +38,24 @@ func GetDefault() *Bbbike {
 				regexp.MustCompile(`https://download\.bbbike\.org/osm/bbbike/$`),
 			},
 			FormatDefinition: formats.FormatDefinitions{
-				formats.FormatCSV:            {ID: formats.FormatCSV, Loc: ".osm.csv.xz", ToLoc: ".osm.csv.xz", BasePath: "", BaseURL: ""},
-				formats.FormatGarminOSM:      {ID: formats.FormatGarminOSM, Loc: ".osm.garmin-osm.zip", ToLoc: "", BasePath: "", BaseURL: ""},
-				formats.FormatGarminOnroad:   {ID: formats.FormatGarminOnroad, Loc: ".osm.garmin-onroad-latin1.zip", ToLoc: "", BasePath: "", BaseURL: ""},
-				formats.FormatGarminOntrail:  {ID: formats.FormatGarminOntrail, Loc: ".osm.garmin-ontrail-latin1.zip", ToLoc: "", BasePath: "", BaseURL: ""},
-				formats.FormatGarminOpenTopo: {ID: formats.FormatGarminOpenTopo, Loc: ".osm.garmin-opentopo-latin1.zip", ToLoc: "", BasePath: "", BaseURL: ""},
-				formats.FormatGeoJSON:        {ID: formats.FormatGeoJSON, Loc: ".osm.geojson.xz", ToLoc: ".geojson.xz", BasePath: "", BaseURL: ""},
-				formats.FormatMBTiles:        {ID: formats.FormatMBTiles, Loc: ".osm.mbtiles-openmaptiles.zip", ToLoc: "osm.mbtiles-openmaptiles.zip", BasePath: "", BaseURL: ""}, //nolint:lll // I don't want to split this line
-				formats.FormatMapsforge:      {ID: formats.FormatMapsforge, Loc: ".osm.mapsforge-osm.zip", ToLoc: "", BasePath: "", BaseURL: ""},
-				formats.FormatOsmGz:          {ID: formats.FormatOsmGz, Loc: ".osm.gz", ToLoc: "", BasePath: "", BaseURL: ""},
-				formats.FormatOsmPbf:         {ID: formats.FormatOsmPbf, Loc: ".osm.pbf", ToLoc: "", BasePath: "", BaseURL: ""},
-				formats.FormatPoly:           {ID: formats.FormatPoly, Loc: ".poly", ToLoc: "", BasePath: "", BaseURL: ""},
-				formats.FormatShpZip:         {ID: formats.FormatShpZip, Loc: ".osm.shp.zip", ToLoc: "", BasePath: "", BaseURL: ""},
+				formats.FormatCSV:            {ID: formats.FormatCSV, Loc: ".osm.csv.xz", ToLoc: ".osm.csv.xz"},
+				formats.FormatGarminOSM:      {ID: formats.FormatGarminOSM, Loc: ".osm.garmin-osm.zip"},
+				formats.FormatGarminOnroad:   {ID: formats.FormatGarminOnroad, Loc: ".osm.garmin-onroad-latin1.zip"},
+				formats.FormatGarminOntrail:  {ID: formats.FormatGarminOntrail, Loc: ".osm.garmin-ontrail-latin1.zip"},
+				formats.FormatGarminOpenTopo: {ID: formats.FormatGarminOpenTopo, Loc: ".osm.garmin-opentopo-latin1.zip"},
+				formats.FormatGeoJSON:        {ID: formats.FormatGeoJSON, Loc: ".osm.geojson.xz", ToLoc: ".geojson.xz"},
+				formats.FormatMBTiles:        {ID: formats.FormatMBTiles, Loc: ".osm.mbtiles-openmaptiles.zip", ToLoc: "osm.mbtiles-openmaptiles.zip"},
+				formats.FormatMapsforge:      {ID: formats.FormatMapsforge, Loc: ".osm.mapsforge-osm.zip"},
+				formats.FormatOsmGz:          {ID: formats.FormatOsmGz, Loc: ".osm.gz"},
+				formats.FormatOsmPbf:         {ID: formats.FormatOsmPbf, Loc: ".osm.pbf"},
+				formats.FormatPoly:           {ID: formats.FormatPoly, Loc: ".poly"},
+				formats.FormatShpZip:         {ID: formats.FormatShpZip, Loc: ".osm.shp.zip"},
 			},
 		},
 	}
 }
 
-// Collector represent geofabrik's scrapper.
+// Collector represents Bbbike's scrapper.
 func (b *Bbbike) Collector() *colly.Collector {
 	myCollector := b.Scrapper.Collector()
 	myCollector.OnHTML("div.list tbody", func(e *colly.HTMLElement) {
@@ -61,23 +68,26 @@ func (b *Bbbike) Collector() *colly.Collector {
 	return myCollector
 }
 
+// ParseList parses the list of elements from the HTML.
 func (b *Bbbike) ParseList(e *colly.HTMLElement, c *colly.Collector) {
 	e.ForEach("a", func(_ int, el *colly.HTMLElement) {
 		href := el.Request.AbsoluteURL(el.Attr("href"))
 		log.Debugf("Parse: %s", href)
 
-		if err := c.Visit(href); err != nil && !errors.Is(err, colly.ErrNoURLFiltersMatch) { // Not matching
+		if err := c.Visit(href); err != nil && !errors.Is(err, colly.ErrNoURLFiltersMatch) {
 			log.WithError(err).Error("can't get url")
 		}
 	})
 }
 
+// GetName extracts the name from the given string.
 func GetName(h3 string) string {
-	ret := h3[17:] // remove "OSM extracts for "
+	const prefixLength = 17
 
-	return ret
+	return h3[prefixLength:] // remove "OSM extracts for "
 }
 
+// ParseSidebar parses the sidebar information from the HTML.
 func (b *Bbbike) ParseSidebar(e *colly.HTMLElement, _ *colly.Collector) {
 	name := GetName(e.ChildText("h3"))
 	myElement := element.Element{
